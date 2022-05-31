e1a3b36073bcb64c8a7090a1b59d937e40aabaa9
Lzzzzzt <liuzitao0123@gmail.com>
2022-05-31 10:57:04 +0800

    修复小BUG

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import router from '@/router'
 import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
 
-require('./mock/index.ts')
+// require('./mock/index.ts')
 
 createApp(App)
   .use(router)
